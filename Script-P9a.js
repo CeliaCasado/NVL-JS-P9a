399116f3ba0ecfd3fd5a2a8888e46080157834c7
Josema <josema@MacBook-Pro-de-Josema.local>
2019-03-05 21:58:40 +0100

    Subida la práctica de matrices I

---
+++ Script-P9a.js
@@ -0,0 +1,10 @@
+	var array = [[1, 2], ["primer_elemento", "segundo_elemento"], [3, 8], [10, 6]];
+	//He definido el array
+	alert (array.length);
+	//Pido que muestre la longitud del array
+	alert (array[1].length);
+	//He pedido que muestre la longitud del segundo elemento del array
+	document.write("El valor del tercer elemento de la lisa del array es" + " " + (array[2]) + " " + "y el segundo elemento del tercer elemento de la lista es" + " " +(array[2][1]));
+	//Muestro por pantalla el valor del tercer elemento de la lista del array. Y dentro de este, su segundo elemento
+
+
